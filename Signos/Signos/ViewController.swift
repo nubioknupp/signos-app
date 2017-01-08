c3d2675af3018ab2ff8ea1d712fea8b7bbbc1994
Núbio Knupp <nubioknupp@gmail.com>
2017-01-08 11:28:12 -0200

    Adicionei uma função para lista signos e exibir detalhes.

---
+++ Signos/Signos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Signos
+//
+//  Created by Nubio Knupp on 15/11/16.
+//  Copyright © 2016 Nubio Knupp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
